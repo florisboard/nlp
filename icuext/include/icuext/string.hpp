c4afce653c465d9f09fc2d7fe565f08a1e7090d8
Patrick Goldinger <patrick@patrickgold.dev>
2022-09-20 16:38:56 +0200

    Implement base of utility lib `icuext`

---
+++ icuext/include/icuext/string.hpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2022 Patrick Goldinger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __FLORISNLP_ICUEXT_USTRING_H__
+#define __FLORISNLP_ICUEXT_USTRING_H__
+
+#include <unicode/locid.h>
+#include <unicode/umachine.h>
+#include <unicode/unistr.h>
+#include <unicode/ustream.h>
+#include <unicode/utypes.h>
+
+#include <string>
+
+namespace icuext {
+
+using u8char = char;
+using u8str = std::basic_string<u8char>;
+using u8str_view = std::basic_string_view<u8char>;
+
+using u16char = UChar;
+using u16str = std::basic_string<u16char>;
+using u16str_view = std::basic_string_view<u16char>;
+
+using u32char = UChar32;
+using u32str = std::basic_string<u32char>;
+using u32str_view = std::basic_string_view<u32char>;
+
+using unistr = icu::UnicodeString;
+using locale = icu::Locale;
+
+namespace str {
+
+void to_unistr(const u8str& src, unistr& dst) noexcept;
+void to_unistr(const u16str& src, unistr& dst) noexcept;
+void to_unistr(const u32str& src, unistr& dst) noexcept;
+unistr to_unistr(const u8str& src) noexcept;
+unistr to_unistr(const u16str& src) noexcept;
+unistr to_unistr(const u32str& src) noexcept;
+
+void to_u8str(const unistr& src, u8str& dst) noexcept;
+void to_u8str(const u16str& src, u8str& dst) noexcept;
+void to_u8str(const u32str& src, u8str& dst) noexcept;
+u8str to_u8str(const unistr& src) noexcept;
+u8str to_u8str(const u16str& src) noexcept;
+u8str to_u8str(const u32str& src) noexcept;
+
+void to_u16str(const unistr& src, u16str& dst) noexcept;
+void to_u16str(const u8str& src, u16str& dst) noexcept;
+void to_u16str(const u32str& src, u16str& dst) noexcept;
+u16str to_u16str(const unistr& src) noexcept;
+u16str to_u16str(const u8str& src) noexcept;
+u16str to_u16str(const u32str& src) noexcept;
+
+void to_u32str(const unistr& src, u32str& dst) noexcept;
+void to_u32str(const u8str& src, u32str& dst) noexcept;
+void to_u32str(const u16str& src, u32str& dst) noexcept;
+u32str to_u32str(const unistr& src) noexcept;
+u32str to_u32str(const u8str& src) noexcept;
+u32str to_u32str(const u16str& src) noexcept;
+
+u8str to_uppercase(const u8str& str) noexcept;
+u16str to_uppercase(const u16str& str) noexcept;
+u32str to_uppercase(const u32str& str) noexcept;
+u8str to_uppercase(const u8str& str, const locale& locale) noexcept;
+u16str to_uppercase(const u16str& str, const locale& locale) noexcept;
+u32str to_uppercase(const u32str& str, const locale& locale) noexcept;
+
+u8str to_lowercase(const u8str& str) noexcept;
+u16str to_lowercase(const u16str& str) noexcept;
+u32str to_lowercase(const u32str& str) noexcept;
+u8str to_lowercase(const u8str& str, const locale& locale) noexcept;
+u16str to_lowercase(const u16str& str, const locale& locale) noexcept;
+u32str to_lowercase(const u32str& str, const locale& locale) noexcept;
+
+void uppercase(u8str& str) noexcept;
+void uppercase(u16str& str) noexcept;
+void uppercase(u32str& str) noexcept;
+void uppercase(u8str& str, const locale& locale) noexcept;
+void uppercase(u16str& str, const locale& locale) noexcept;
+void uppercase(u32str& str, const locale& locale) noexcept;
+void lowercase(u8str& str) noexcept;
+void lowercase(u16str& str) noexcept;
+void lowercase(u32str& str) noexcept;
+void lowercase(u8str& str, const locale& locale) noexcept;
+void lowercase(u16str& str, const locale& locale) noexcept;
+void lowercase(u32str& str, const locale& locale) noexcept;
+
+} // namespace str
+
+} // namespace icuext
+
+#endif
---
+++ icuext/src/data.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 Patrick Goldinger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "icuext/data.hpp"
+
+#include <unicode/udata.h>
+
+#include <fstream>
+
+UErrorCode icuext::load_and_set_common_data(std::string path) {
+    std::ifstream in_file(path, std::ios::in | std::ios::binary);
+    if (!in_file) {
+        return U_FILE_ACCESS_ERROR;
+    }
+    in_file.seekg(0, std::ios::end);
+    size_t size = in_file.tellg();
+    if (size <= 0) {
+        return U_FILE_ACCESS_ERROR;
+    }
+    in_file.seekg(0, std::ios::beg);
+    char* icu_data = new char[size + 1];
+    in_file.read(icu_data, size);
+    if (!in_file) {
+        delete[] icu_data;
+        in_file.close();
+        return U_FILE_ACCESS_ERROR;
+    }
+    icu_data[size] = 0;
+    in_file.close();
+    UErrorCode status = U_ZERO_ERROR;
+    udata_setCommonData(reinterpret_cast<void*>(icu_data), &status);
+    return status;
+}
