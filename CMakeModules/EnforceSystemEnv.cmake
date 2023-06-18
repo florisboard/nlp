531824ee1bed01d12adbae905fc44a3c9e4c587d
Patrick Goldinger <patrick@patrickgold.dev>
2023-06-18 19:40:24 +0200

    Improve system env detection script

--- CMakeModules/EnforceSystemEnv.cmake
+++ CMakeModules/EnforceSystemEnv.cmake
@@ -1,14 +1,15 @@
 if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
-    message(FATAL_ERROR "Unsupported host platform: Windows. Consider using a Linux machine or using WSL2.")
+    message(FATAL_ERROR "Unsupported host platform: Windows.\nConsult the README for a list of supported host systems.\n")
 elseif (CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
     # Supported
 elseif (CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
     # Supported
 else ()
-    message(FATAL_ERROR "Unknown host platform: ${CMAKE_HOST_SYSTEM_NAME}. Consider using a Linux machine.")
+    message(FATAL_ERROR "Unknown host platform: ${CMAKE_HOST_SYSTEM_NAME}\nConsult the README for a list of supported host systems.\n"
+        "If you run a supported host system but you still get this message please file an issue at https://github.com/florisboard/nlp/issues\n")
 endif()
 
-if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
     # Supported
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     message(FATAL_ERROR "Unsupported compiler: GCC")
